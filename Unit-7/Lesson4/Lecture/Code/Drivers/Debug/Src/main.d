02c44594a0b0b3002f4da04c054b85a974b9219f
Ahmed Eldeeb <aeldeeb55@gmail.com>
2023-10-12 23:48:54 +0300

    Lesson4

---
+++ Unit-7/Lesson4/Lecture/Code/Drivers/Debug/Src/main.d
@@ -0,0 +1,10 @@
+Src/main.o: ../Src/main.c \
+ D:/Master_Embedded_Diploma/1-Important/Eclipse/Drivers/Stm32_F103C6_Drivers/Includes/STM32_F103C6.h \
+ D:/Master_Embedded_Diploma/1-Important/Eclipse/Drivers/Stm32_F103C6_Drivers/Includes/GPIO_driver.h \
+ D:/Master_Embedded_Diploma/1-Important/Eclipse/Drivers/Stm32_F103C6_Drivers/Includes/STM32_F103C6.h
+
+D:/Master_Embedded_Diploma/1-Important/Eclipse/Drivers/Stm32_F103C6_Drivers/Includes/STM32_F103C6.h:
+
+D:/Master_Embedded_Diploma/1-Important/Eclipse/Drivers/Stm32_F103C6_Drivers/Includes/GPIO_driver.h:
+
+D:/Master_Embedded_Diploma/1-Important/Eclipse/Drivers/Stm32_F103C6_Drivers/Includes/STM32_F103C6.h:
---
+++ Unit-7/Lesson4/Lecture/Code/Drivers/Stm32_F103C6_Drivers/Includes/STM32_F103C6.h
@@ -0,0 +1,173 @@
+/*
+ * STM32_F103C6.h
+ *
+ *  Created on: Oct 5, 2023
+ *      Author: Eldeeb
+ */
+
+#ifndef INCLUDES_STM32_F103C6_H_
+#define INCLUDES_STM32_F103C6_H_
+
+//-----------------------------
+//Includes
+//-----------------------------
+
+#include "stdlib.h"
+#include <stdint.h>
+
+
+
+//-----------------------------
+//Base addresses for Memories
+//-----------------------------
+
+#define FLASH_MEMORY_BASE 						0x08000000UL
+#define SYSTEM_MEMORY_BASE 						0x1FFFF000UL
+#define SRAM_MEMORY_BASE 						0x20000000UL
+
+#define PERIPHRALS_BASE 						0x40000000UL
+
+#define CORTEX_M3_Internal_Periphrals_BASE 		0xE0000000UL
+
+//-----------------------------
+//Base addresses for AHB Peripherals
+//-----------------------------
+
+
+//RCC
+#define RCC_BASE								0x40021000UL
+
+//-----------------------------
+//Base addresses for APB2 Peripherals
+//-----------------------------
+
+//GPIO
+//A,B fully included in LQFP48 Package
+#define GPIOA_BASE								0x40010800UL
+#define GPIOB_BASE								0x40010C00UL
+
+
+//C,D partially included in LQFP48 Package
+#define GPIOC_BASE								0x40011000UL
+#define GPIOD_BASE								0x40011400UL
+//E  not included in LQFP48 Package
+#define GPIOE_BASE								0x40011800UL
+
+
+//EXTI
+#define EXTI_BASE 								0x40010400UL
+
+//AFIO
+#define AFIO_BASE 								0x40010000UL
+
+
+//-----------------------------
+//Base addresses for APB2 Peripherals
+//-----------------------------
+
+
+
+//==================================================================
+
+
+//-*-*-*-*-*-*-*-*-*-*-*-
+//Peripheral register:GPIO
+//-*-*-*-*-*-*-*-*-*-*-*
+
+typedef struct
+{
+	volatile uint32_t CRL;
+	volatile uint32_t CRH;
+	volatile uint32_t IDR;
+	volatile uint32_t ODR;
+	volatile uint32_t BSRR;
+	volatile uint32_t BRR;
+	volatile uint32_t LCKR;
+}GPIO_Typedef;
+
+
+
+//-*-*-*-*-*-*-*-*-*-*-*-
+//Peripheral register:RCC
+//-*-*-*-*-*-*-*-*-*-*-*
+typedef struct
+{
+	volatile uint32_t CR;
+	volatile uint32_t CFGR;
+	volatile uint32_t CIR;
+	volatile uint32_t APB2RSTR;
+	volatile uint32_t APB1RSTR;
+	volatile uint32_t ABHBENRRR;
+	volatile uint32_t APB2ENR;
+	volatile uint32_t APB1ENR;
+	volatile uint32_t BDCR;
+	volatile uint32_t CSR;
+}RCC_Typedef;
+
+
+
+//-*-*-*-*-*-*-*-*-*-*-*-
+//Peripheral register:EXTI
+//-*-*-*-*-*-*-*-*-*-*-*
+typedef struct
+{
+	volatile uint32_t IMR;
+	volatile uint32_t EMR;
+	volatile uint32_t RTSR;
+	volatile uint32_t FTSR;
+	volatile uint32_t SWIER;
+	volatile uint32_t PR;
+}EXTI_Typedef;
+
+//-*-*-*-*-*-*-*-*-*-*-*-
+//Peripheral register:AFIO
+//-*-*-*-*-*-*-*-*-*-*-*
+typedef struct
+{
+	volatile uint32_t EVCR;
+	volatile uint32_t MAPR;
+	volatile uint32_t EXTICR1;
+	volatile uint32_t EXTICR2;
+	volatile uint32_t EXTICR3;
+			 uint32_t RESERVED0;//0x18
+	volatile uint32_t MAPR2;
+}AFIO_Typedef;
+
+//=================================================================
+
+//-*-*-*-*-*-*-*-*-*-*-*-
+//Peripheral Instants:
+//-*-*-*-*-*-*-*-*-*-*-*
+
+#define GPIOA 				((GPIO_Typedef*)GPIOA_BASE)
+#define GPIOB				((GPIO_Typedef*)GPIOB_BASE)
+#define GPIOC 				((GPIO_Typedef*)GPIOC_BASE)
+#define GPIOD 				((GPIO_Typedef*)GPIOD_BASE)
+#define GPIOE 				((GPIO_Typedef*)GPIOE_BASE)
+
+#define RCC 				((RCC_Typedef*)RCC_BASE)
+
+#define EXTI				((EXTI_Typedef*)EXTI_BASE)
+
+#define AFIO				((AFIO_Typedef*)AFIO_BASE)
+
+
+//===========================================================
+//-*-*-*-*-*-*-*-*-*-*-*-
+//clock enable Macros:
+//-*-*-*-*-*-*-*-*-*-*-*
+
+#define RCC_GPIOA_CLK_EN()			(RCC->APB2ENR |= 1<<2)
+#define RCC_GPIOB_CLK_EN()			(RCC->APB2ENR |= 1<<3)
+#define RCC_GPIOC_CLK_EN()			(RCC->APB2ENR |= 1<<4)
+#define RCC_GPIOD_CLK_EN()			(RCC->APB2ENR |= 1<<5)
+#define RCC_GPIOE_CLK_EN()			(RCC->APB2ENR |= 1<<6)
+
+#define RCC_AFIO_CLK_EN()			(RCC->APB2ENR |= 1<<0)
+
+
+//-*-*-*-*-*-*-*-*-*-*-*-
+//Generic Macros:
+//-*-*-*-*-*-*-*-*-*-*-*
+
+#endif /* INCLUDES_STM32_F103C6_H_ */

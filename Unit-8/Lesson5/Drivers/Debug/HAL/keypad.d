b7e154328f96718676d42c52b7274c8ce55f8bf6
Ahmed Eldeeb <aeldeeb55@gmail.com>
2024-02-23 21:27:30 +0200

    Lesson5

---
+++ Unit-8/Lesson5/Drivers/Debug/HAL/keypad.d
@@ -0,0 +1,13 @@
+HAL/keypad.o: ../HAL/keypad.c \
+ D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson4/Drivers/HAL/includes/keypad.h \
+ D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson4/Drivers/Stm32_F103C6_Drivers/inc/STM32F103x8.h \
+ D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson4/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_gpio_driver.h \
+ D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson4/Drivers/Stm32_F103C6_Drivers/inc/STM32F103x8.h
+
+D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson4/Drivers/HAL/includes/keypad.h:
+
+D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson4/Drivers/Stm32_F103C6_Drivers/inc/STM32F103x8.h:
+
+D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson4/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_gpio_driver.h:
+
+D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson4/Drivers/Stm32_F103C6_Drivers/inc/STM32F103x8.h:
---
+++ Unit-8/Lesson5/Drivers/Debug/Stm32_F103C6_Drivers/subdir.mk
@@ -0,0 +1,34 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Stm32_F103C6_Drivers/stm32f103x8_EXTI_driver.c \
+../Stm32_F103C6_Drivers/stm32f103x8_RCC_driver.c \
+../Stm32_F103C6_Drivers/stm32f103x8_SPI_driver.c \
+../Stm32_F103C6_Drivers/stm32f103x8_USART_driver.c 
+
+OBJS += \
+./Stm32_F103C6_Drivers/stm32f103x8_EXTI_driver.o \
+./Stm32_F103C6_Drivers/stm32f103x8_RCC_driver.o \
+./Stm32_F103C6_Drivers/stm32f103x8_SPI_driver.o \
+./Stm32_F103C6_Drivers/stm32f103x8_USART_driver.o 
+
+C_DEPS += \
+./Stm32_F103C6_Drivers/stm32f103x8_EXTI_driver.d \
+./Stm32_F103C6_Drivers/stm32f103x8_RCC_driver.d \
+./Stm32_F103C6_Drivers/stm32f103x8_SPI_driver.d \
+./Stm32_F103C6_Drivers/stm32f103x8_USART_driver.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Stm32_F103C6_Drivers/stm32f103x8_EXTI_driver.o: ../Stm32_F103C6_Drivers/stm32f103x8_EXTI_driver.c
+	arm-none-eabi-gcc  -gdwarf-2 "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I../Inc -I"D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson4/Drivers/HAL/includes" -I"D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson4/Drivers/Stm32_F103C6_Drivers/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Stm32_F103C6_Drivers/stm32f103x8_EXTI_driver.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Stm32_F103C6_Drivers/stm32f103x8_RCC_driver.o: ../Stm32_F103C6_Drivers/stm32f103x8_RCC_driver.c
+	arm-none-eabi-gcc  -gdwarf-2 "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I../Inc -I"D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson4/Drivers/HAL/includes" -I"D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson4/Drivers/Stm32_F103C6_Drivers/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Stm32_F103C6_Drivers/stm32f103x8_RCC_driver.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Stm32_F103C6_Drivers/stm32f103x8_SPI_driver.o: ../Stm32_F103C6_Drivers/stm32f103x8_SPI_driver.c
+	arm-none-eabi-gcc  -gdwarf-2 "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I../Inc -I"D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson4/Drivers/HAL/includes" -I"D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson4/Drivers/Stm32_F103C6_Drivers/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Stm32_F103C6_Drivers/stm32f103x8_SPI_driver.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Stm32_F103C6_Drivers/stm32f103x8_USART_driver.o: ../Stm32_F103C6_Drivers/stm32f103x8_USART_driver.c
+	arm-none-eabi-gcc  -gdwarf-2 "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I../Inc -I"D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson4/Drivers/HAL/includes" -I"D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson4/Drivers/Stm32_F103C6_Drivers/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Stm32_F103C6_Drivers/stm32f103x8_USART_driver.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+

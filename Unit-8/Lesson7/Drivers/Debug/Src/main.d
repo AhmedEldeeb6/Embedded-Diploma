41910566cbc4b3cc567c9f3a4f220bd24ce2b56b
Ahmed Eldeeb <aeldeeb55@gmail.com>
2024-03-18 19:26:15 +0200

    Lesson7

---
+++ Unit-8/Lesson7/Drivers/Debug/Src/main.d
@@ -0,0 +1,25 @@
+Src/main.o: ../Src/main.c \
+ D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_gpio_driver.h \
+ D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/STM32F103x8.h \
+ D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/STM32F103x8.h \
+ D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_EXTI_driver.h \
+ D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_gpio_driver.h \
+ D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_USART_driver.h \
+ D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_RCC_driver.h \
+ D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_SPI_driver.h
+
+D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_gpio_driver.h:
+
+D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/STM32F103x8.h:
+
+D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/STM32F103x8.h:
+
+D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_EXTI_driver.h:
+
+D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_gpio_driver.h:
+
+D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_USART_driver.h:
+
+D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_RCC_driver.h:
+
+D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_SPI_driver.h:
---
+++ Unit-8/Lesson7/Drivers/Debug/Stm32_F103C6_Drivers/stm32f103x8_I2C_driver.d
@@ -0,0 +1,14 @@
+Stm32_F103C6_Drivers/stm32f103x8_I2C_driver.o: \
+ ../Stm32_F103C6_Drivers/stm32f103x8_I2C_driver.c \
+ D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_I2C_driver.h \
+ D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/STM32F103x8.h \
+ D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_gpio_driver.h \
+ D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_RCC_driver.h
+
+D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_I2C_driver.h:
+
+D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/STM32F103x8.h:
+
+D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_gpio_driver.h:
+
+D:/Master_Embedded_Diploma/1-Important/Embedded-Diploma/Unit-8/Lesson7/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x8_RCC_driver.h:
